966c487f2bf6b73c51899e1b5c07dae8f8abd1ab
os <oli.schmuhl@cantaa.de>
2015-12-05 20:35:44 +0100

    #0000 - change: component.js, update of documentation - change: composite.js, update of documentation - change: display.js, update of documentation - change: dom.js, update of documentation - change: method Dom::ExistCssClass, update of documentation - change: module.js, update of documentation - change: resources.js, update of documentation - change: runtime.js, update of documentation

--- sources/display.js
+++ sources/display.js
@@ -1,4 +1,27 @@
 /**
+ *  LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt,
+ *  im Folgenden Seanox Software Solutions oder kurz Seanox genannt. Diese
+ *  Software unterliegt der Version 2 der GNU General Public License.
+ *
+ *  Seanox Aspect, Aspect Oriented Publishing
+ *  Copyright (C) 2015 Seanox Software Solutions
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of version 2 of the GNU General Public License as published
+ *  by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *  more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *
+ *      BESCHREIBUNG
+ *
  *  Display stellt Methoden fuer den Umgang mit der Anzeige bereit.
  *  
  *  Display 1.0 20150123
--- sources/dom.js
+++ sources/dom.js
@@ -1,4 +1,27 @@
 /**
+ *  LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt,
+ *  im Folgenden Seanox Software Solutions oder kurz Seanox genannt. Diese
+ *  Software unterliegt der Version 2 der GNU General Public License.
+ *
+ *  Seanox Aspect, Aspect Oriented Publishing
+ *  Copyright (C) 2015 Seanox Software Solutions
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of version 2 of the GNU General Public License as published
+ *  by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *  more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *
+ *      BESCHREIBUNG
+ *
  *  Dom stellt erweiterte Methoden fuer den Zugriff auf das DOM bereit.
  *  
  *  Dom 1.0 20150123
@@ -6,7 +29,7 @@
  *  Alle Rechte vorbehalten.
  *
  *  @author  Seanox Software Solutions
- *  @version 1.0 20150123
+ *  @version 1.0.1 20151205
  */
 if (typeof(Dom) == 'undefined') {
 
@@ -189,7 +212,7 @@ if (typeof(Dom) == 'undefined') {
             css = css.split(" ");
 
         for (loop = 0; loop < css.length; loop++)
-            if (!object.className.match(new RegExp("(?:^|\\s)" + css[loop] + "(?:\\s|$)", "i"))
+            if (!object.className.match(new RegExp("(?:^|\\s)" + css[loop] + "(?:\\s|$)", "i")))
                     return false;
         
         return loop > 0;
