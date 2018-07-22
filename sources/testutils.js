513423556176981432b65c9c7336b2a4c5c4f367
seanox <service@seanox.de>
2018-07-22 21:09:39 +0200

    #0000 Console: Forwarding extension when using IFrames #0000 Assert.assertEqualsTemplate: Spaces at the beginning and end of lines are ignored

--- sources/testutils.js
+++ sources/testutils.js
@@ -24,12 +24,12 @@
  *      ----
  *  Project-specific extensions of the test environment.
  *  
- *  TestUtils 1.0 20180421
+ *  TestUtils 1.0 20180722
  *  Copyright (C) 2018 Seanox Software Solutions
  *  Alle Rechte vorbehalten.
  *
  *  @author  Seanox Software Solutions
- *  @version 1.0 20180421
+ *  @version 1.0 20180722
  */
 if (typeof(TestUtils) === "undefined") {
     
@@ -39,13 +39,18 @@ if (typeof(TestUtils) === "undefined") {
 /**
  *  Enhancement of the Test/Assert API
  *  Adds a equals methode for a template to the Assert objects.
+ *  Spaces at the beginning and end of lines are ignored.
  */ 
 if (Assert.assertEqualsTemplate === undefined)
     Assert.assertEqualsTemplate = function(selector, actual) {
         var element = document.querySelector(selector);
         var content = element.innerHTML.trim().replace(/\t/g, "    ");
+        content = content.replace(/(\r\n)|(\n\r)|[\r\n]/gm, "\n");
+        content = content.replace(/(^\s+)|(\s+$)/gm, "");
         actual = actual.trim();
         actual = actual.replace(/\t/g, "    ");
+        actual = actual.replace(/(\r\n)|(\n\r)|[\r\n]/gm, "\n");
+        actual = actual.replace(/(^\s+)|(\s+$)/gm, "");
         Assert.assertEquals(content, actual);    
     };
    
@@ -68,4 +73,73 @@ if (Element.prototype.typeValue === undefined) {
             element.dispatchEvent(eventKeyUp);
         });
     };     
+};
+
+/**
+ *  Implementation of a redirection of the console when using tests in IFrames.
+ *  Redirection  is based on calling message events in the parent document.
+ *  The message events are on methods for console levels: INFO, ERROR, WARN, LOG.
+ *  
+ *      Example:
+ *      
+ *  var onLog = function(message) {
+ *      ....
+ *  }
+ */
+if (typeof(parent) !== "undefined") {
+    
+    /** 
+     *  General method for redirecting console levels.
+     *  @param level
+     *  @param variants
+     */
+    console.forward = function(level, variants) {
+        
+        var invoke;
+        if (parent)
+            invoke = parent["on" + level.capitalize()];
+        if (invoke == null)
+            invoke = console.forward[level];
+        invoke.apply(null, variants);
+    };
+    
+    /** Redirect for the level: LOG */
+    console.forward.log = console.log;
+    console.log = function(message) {
+        console.forward("log", arguments);
+    };
+    
+    /** Redirect for the level: WARN */
+    console.forward.warn = console.warn;
+    console.warn = function(message) {
+        console.forward("warn", arguments);
+    };
+    
+    /** Redirect for the level: ERROR */
+    console.forward.error = console.error;
+    console.error = function(message) {
+        console.forward("error", arguments);
+    };
+    
+    /** Redirect for the level: INFO */
+    console.forward.info = console.info;
+    console.info = function(message) {
+        console.forward("info", arguments);
+    };
+    
+    /** Registration of events for redirection */
+    if (typeof(parent.onFinish) === "function")
+        Test.listen(Test.EVENT_FINISH, parent.onFinish);
+    if (typeof(parent.onInterrupt) === "function")
+        Test.listen(Test.EVENT_INTERRUPT, parent.onInterrupt);
+    if (typeof(parent.onPerform) === "function")
+        Test.listen(Test.EVENT_PERFORM, parent.onPerform);
+    if (typeof(parent.onResponse) === "function")
+        Test.listen(Test.EVENT_RESPONSE, parent.onResponse);
+    if (typeof(parent.onResume) === "function")
+        Test.listen(Test.EVENT_RESUME, parent.onResume);
+    if (typeof(parent.onStart) === "function")
+        Test.listen(Test.EVENT_START, parent.onStart);
+    if (typeof(parent.onSuspend) === "function")    
+        Test.listen(Test.EVENT_SUSPEND, parent.onSuspend);
 };
