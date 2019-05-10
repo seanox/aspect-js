17d209b4a09d31ddd71081af133e006160aea7c2
seanox <service@seanox.de>
2019-05-10 20:47:01 +0200

    #0000 Test: Update of test cases

--- test/modules/common.js
+++ test/modules/common.js
@@ -1,11 +1,15 @@
 if (navigator.userAgentType === undefined) {
     navigator.userAgentType = navigator.userAgent.match(/edge/i);
-    if (!navigator.userAgentType)
-        navigator.userAgentType = navigator.userAgent.match(/chrome/i);
-    if (!navigator.userAgentType)
-        navigator.userAgentType = navigator.userAgent.match(/safari/i);
+    if (!navigator.userAgentType
+            && navigator.userAgent.match(/version.*safari/i))
+        navigator.userAgentType = "safari";
     if (!navigator.userAgentType)
         navigator.userAgentType = navigator.userAgent.match(/firefox/i);
+    if (!navigator.userAgentType
+            && navigator.userAgent.match(/QupZilla/i))
+        navigator.userAgentType = "QT";
+    if (!navigator.userAgentType)
+        navigator.userAgentType = navigator.userAgent.match(/chrome/i);
     if (navigator.userAgentType)
         navigator.userAgentType = String(navigator.userAgentType).toLowerCase();
 };
