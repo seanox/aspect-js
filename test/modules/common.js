fd30801c68581281c8d2213cb55fdcbcb7b9118c
seanox <service@seanox.de>
2019-05-11 11:39:31 +0200

    #0000 Test: Update of test cases

--- test/modules/common.js
+++ test/modules/common.js
@@ -1,15 +1,42 @@
-if (navigator.userAgentType === undefined) {
-    navigator.userAgentType = navigator.userAgent.match(/edge/i);
-    if (!navigator.userAgentType
+if (navigator.engine === undefined) {
+    navigator.engine = navigator.userAgent.match(/edge/i);
+    if (!navigator.engine
             && navigator.userAgent.match(/version.*safari/i))
-        navigator.userAgentType = "safari";
-    if (!navigator.userAgentType)
-        navigator.userAgentType = navigator.userAgent.match(/firefox/i);
-    if (!navigator.userAgentType
-            && navigator.userAgent.match(/QupZilla/i))
-        navigator.userAgentType = "QT";
-    if (!navigator.userAgentType)
-        navigator.userAgentType = navigator.userAgent.match(/chrome/i);
-    if (navigator.userAgentType)
-        navigator.userAgentType = String(navigator.userAgentType).toLowerCase();
+        navigator.engine = "webkit";
+    if (!navigator.engine
+            && navigator.userAgent.match(/like gecko/i)
+            && !navigator.userAgent.match(/\) chrome\/[\d\.]+ safari\/[\d\.]+$/i))
+        navigator.engine = "blink";
+    if (!navigator.engine)
+        navigator.engine = navigator.userAgent.match(/goanna/i);
+    if (!navigator.engine
+            && navigator.userAgent.match(/gecko\/[\d\.]+\b/i))
+        navigator.engine = "gecko";
+    if (!navigator.engine)
+        navigator.engine = navigator.userAgent.match(/chrome/i);
+    if (navigator.engine)
+        navigator.engine = String(navigator.engine).toLowerCase();
+};
+
+if (Test.read === undefined) {
+    Test.read = function(content) {
+        var request = new XMLHttpRequest();
+        request.overrideMimeType("text/plain");
+        request.open("GET", content, false);
+        request.send();
+        if (request.status != "200")
+            throw Error("HTTP status " + request.status + " for " + url);
+        return request.responseText;
+    };
+};
+
+Assert.assertSameText = function(expected, actual) {
+    expected = (expected || "").trim().replace(/\t/g, "    ");
+    expected = expected.replace(/(\r\n)|(\n\r)|[\r\n]/gm, "\n");
+    expected = expected.replace(/(^\s+)|(\s+$)/gm, "");
+    actual = actual.trim();
+    actual = actual.replace(/\t/g, "    ");
+    actual = actual.replace(/(\r\n)|(\n\r)|[\r\n]/gm, "\n");
+    actual = actual.replace(/(^\s+)|(\s+$)/gm, "");
+    Assert.assertEquals(expected, actual);    
 };
