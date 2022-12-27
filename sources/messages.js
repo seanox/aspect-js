77a95c1d9bbe0478aa7d946776622cbc008128ef
seanox <service@seanox.de>
2022-12-27 09:48:51 +0100

    #0000 Review: Optimization and corrections

--- sources/messages.js
+++ sources/messages.js
@@ -4,7 +4,7 @@
  * Diese Software unterliegt der Version 2 der Apache License.
  *
  * Seanox aspect-js, Fullstack JavaScript UI Framework
- * Copyright (C) 2021 Seanox Software Solutions
+ * Copyright (C) 2022 Seanox Software Solutions
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
