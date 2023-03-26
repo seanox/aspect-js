9680f8edaf73d189c389cd8e476878eefe782da3
seanox <service@seanox.de>
2023-03-26 21:14:18 +0200

    #0000 Test: Update of test cases

--- test/modules/common.js
+++ test/modules/common.js
@@ -1,16 +1,21 @@
-if (navigator.engine === undefined) {
+(() => {
+    compliant("navigator.engine");
+    let _engine = null;
     if (navigator.userAgent.match(/Mozilla\/[\d\.]+ \(.*\) AppleWebKit\/[\d\.]+ \(KHTML, like Gecko\).* Safari\/[\d\.]+/i))
-        navigator.engine = "webkit";
+        _engine = "webkit";
     if (navigator.userAgent.match(/Mozilla\/[\d\.]+ \(.*\) AppleWebKit\/[\d\.]+ \(KHTML, like Gecko\).* Chrome\/[\d\.]+/i))
-        navigator.engine = "blink";
+        _engine = "blink";
     if (navigator.userAgent.match(/Mozilla\/[\d\.]+ \(.*\) Gecko\/[\d\.]+/i))
-        navigator.engine = "gecko";
+        _engine = "gecko";
     if (navigator.userAgent.match(/Mozilla\/[\d\.]+ \(.*\) Gecko\/[\d\.]+ Goanna\/[\d\.]+/i))
-        navigator.engine = "goanna";
-}
+        _engine = "goanna";
+    if (_engine)
+        Object.defineProperty(navigator, "engine", {
+            value: _engine
+        });
 
-if (Test.read === undefined) {
-    Test.read = function(content) {
+    compliant("Test.read");
+    compliant(null, Test.read = (content) => {
         const request = new XMLHttpRequest();
         request.overrideMimeType("text/plain");
         if (content)
@@ -22,45 +27,47 @@ if (Test.read === undefined) {
         if (request.status !== 200)
             throw new Error("HTTP status " + request.status + " for " + request.responseURL);
         return request.responseText;
-    };
-}
+    });
 
-if (String.prototype.simplify === undefined) {
-    String.prototype.simplify = function() {
+    compliant("String.prototype.simplify");
+    compliant(null, String.prototype.simplify = function() {
         return this.trim().replace(/\t/g, "    ")
             .replace(/(\r\n)|(\n\r)|[\r\n]/gm, "\n")
             .replace(/(^ +)|( +$)/gm, "")
             .replace(/\s*\n+\s*\n+/g, "\n")
             .replace(/&gt;/g, ">")
             .replace(/&lt;/g, "<");
-    }
-}
+    });
 
-if (console.test === undefined) {
-    console.test = function(content) {
+    compliant("console.test");
+    compliant(null, console.test = (content) => {
         console.test.count = (console.test.count || 0) +1;
         console.log(`script: #${console.test.count}`);
         console.log(`<script type="text/test">\n${String(content || "").simplify()}\n<\/script>`);
+    });
+
+    // Assert becomes available only in the unit test by activating the Test API
+    if (typeof Assert !== "undefined") {
+
+        compliant("Assert.assertSameText");
+        compliant(null, Assert.assertSameText = (expected, actual) => {
+            Assert.assertEquals(String(expected || "").simplify(), String(actual || "").simplify());
+        });
+
+        compliant("Assert.assertSameTo");
+        compliant(null, Assert.assertSameTo = (selector, actual) => {
+            const element = document.querySelector(selector);
+            const content = element.innerHTML;
+            Assert.assertEquals(content.simplify(), String(actual || "").simplify());
+        });
+
+        compliant("Assert.assertIn");
+        compliant(null, Assert.assertIn = (...variants) => {
+            const assert = Assert.create(variants, 2);
+            if (assert.values[0].includes(assert.values[1]))
+                return;
+            assert.values[0] = "[" + assert.values[0].join(", ") + "]";
+            throw assert.error("Assert.assertIn", "in {0}", "{1}");
+        });
     }
-}
-
-if (typeof Assert !== "undefined") {
-
-    Assert.assertSameText = function(expected, actual) {
-        Assert.assertEquals(String(expected || "").simplify(), String(actual || "").simplify());
-    };
-    
-    Assert.assertSameTo = function(selector, actual) {
-        const element = document.querySelector(selector);
-        const content = element.innerHTML;
-        Assert.assertEquals(content.simplify(), String(actual || "").simplify());
-    };
-    
-    Assert.assertIn = function(values, actual) {
-        const assert = Assert.create(arguments, 2);
-        if (assert.values[0].includes(assert.values[1]))
-            return;
-        assert.values[0] = "[" + assert.values[0].join(", ") + "]";
-        throw assert.error("Assert.assertIn", "in {0}", "{1}");
-    };
-}
+})();
