fc6ec2780d8f12f8584e90fc5ea2a729884ff0f7
seanox <service@seanox.de>
2023-03-27 07:28:39 +0200

    #0000 Test: Update of test cases

--- test/modules/moduleM.js
+++ test/modules/moduleM.js
@@ -2,7 +2,9 @@ const exampleE_A1 = "A-AAA";
 const exampleE_A2 = {value: "A-BBB"};
 const exampleE_A3 = () => {return "A-CCC";};
 const exampleE_A4 = "A-DDD";
-#export exampleE_A1 exampleE_A2
+const exampleE_A5 = "A-EEE";
+#export exampleE_A1 exampleE_A2 exampleE_A5@b.c.d
+#export exampleE_A1@a.b.c
 
 const exampleX_01 = "123\"" + 456 + "789\"";
 const exampleX_02 = "123\'" + 456 + "789\'";
@@ -12,7 +14,6 @@ const exampleX_04 = "123\'" + 456 + "789\"";
 
 if (true) #export exampleE_A3;
 if (false) #export exampleE_A4;
-#export exampleE_A1@a.b.c
 
 let exampleE_B1 = "B-AAA";
 let exampleE_B2 = {value: "B-BBB"};
