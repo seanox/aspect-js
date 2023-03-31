715854933bcfa55a01387e2c1e646f1e1cc55fd0
seanox <service@seanox.de>
2023-03-31 20:43:41 +0200

    #0000 Test: Update of test cases

--- test/modules/syntax.js
+++ test/modules/syntax.js
@@ -7,3 +7,12 @@ const a1 = (?xxx.xxx.xxx);
 const a2 = (?x1());
 
 #export a1 a2;
+
+const c1 = ( ?xxx.xxx.xxx);
+const c2 = ( ?x1());
+const c3 = (
+    ?xxx.xxx.xxx);
+const c4 = (
+    ?x1());
+
+#export c1 c2 c3 c4;
