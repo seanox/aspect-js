402ec9a33669c81666063472270e01c74f9fac32
seanox <service@seanox.de>
2023-03-31 20:20:20 +0200

    #0000 Test: Update of test cases

---
+++ test/modules/syntax.js
@@ -0,0 +1,9 @@
+#import syntaxA
+#import syntax
+
+const x1 = () => {throw new Error("xxx");}
+
+const a1 = (?xxx.xxx.xxx);
+const a2 = (?x1());
+
+#export a1 a2;
---
+++ test/modules/syntaxA.js
@@ -0,0 +1,9 @@
+#import syntaxA
+#import syntax
+
+const x1 = () => {throw new Error("xxx");}
+
+const b1 = (?xxx.xxx.xxx);
+const b2 = (?x1());
+
+#export b1 b2;
