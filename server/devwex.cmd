e26ae4c2c989272aca2f42c58c14c99e7ad6b109
seanox <service@seanox.com>
2024-12-27 08:25:34 +0100

    #0000 Test: Update web server to version 5.7.1

--- server/devwex.cmd
+++ server/devwex.cmd
@@ -1,44 +1,82 @@
 @echo off
 
-rem Seanox Devwex is started from working directory ./devwex/program.
+rem Seanox Devwex is started from working directory .\devwex\program.
 rem Here, following variables are used:
 rem
-rem   CLASSPATH     path of java resources
+rem   OPTIONS  spaces separated arguments for the java virtual machine, in
+rem            format -Dname=value ...
 rem
-rem   JAVAPATH      path of java runtime environment
-rem
-rem   LIBRARIESPATH semicolon separate paths, from which the server invites
-rem                 startup modules
-rem
-rem   OPTIONS       spaces separated arguments for the java virtual machin,
-rem                 in format -Dname=value;...
-rem
-rem   SYSTEMDRIVE   standard variable SYSTEMDRIVE of Windows, this is used of
-rem                 some CGI application to find systems components
-rem
-rem   SYSTEMPATH    based on standard variable PATH of Windows, this is used of
-rem                 some CGI application to find systems components
-rem
-rem   SYSTEMROOT    standard variable SYSTEMROOT of Windows, this is used of
-rem                 some CGI application to find systems components
-rem
-rem With startup all batch scripts are loaded from path ../runtime/scripts.
+rem With startup all batch scripts are loaded from path ..\runtime.
 rem Please note, that scripts to extend of runtime environment are relative to
 rem working directory.
 
 cls
 
-set CLASSPATH=
-set JAVAPATH=
-set LIBRARIESPATH=
+SETLOCAL
+
 set OPTIONS=
-set SYSTEMPATH=%PATH%
+set LIBRARIES=..\libraries
+set RUNTIME=..\runtime
 
-for %%f in (../runtime/scripts/*.bat ../runtime/scripts/*.cmd) do call ../runtime/scripts/%%f
+rem Automatic determination of the Java runtime environment:
+rem - in the runtime sub-directories ..\runtime
+rem - else if JAVA_HOME is set
+rem - else Java runtime in the PATH variable
 
-set OPTIONS=%OPTIONS% -Dpath="%SYSTEMPATH%;"
-set OPTIONS=%OPTIONS% -Dsystemdrive=%SYSTEMDRIVE%
-set OPTIONS=%OPTIONS% -Dsystemroot="%SYSTEMROOT%"
-set OPTIONS=%OPTIONS% -Dlibraries="..\libraries;%LIBRARIESPATH%;"
+SetLocal EnableDelayedExpansion
 
-%JAVAPATH%java -cp "devwex.jar;%CLASSPATH%" %OPTIONS% com.seanox.devwex.Service %1
+set JAVA=
+if exist "%RUNTIME%" (
+  for /f "delims=: " %%d in ('dir /AD /B %RUNTIME%') do (
+    set DIRECTORY=%cd%\%RUNTIME%\%%d
+    set DIRECTORY=!DIRECTORY:\\=\!
+    set PATH=!DIRECTORY!;!PATH!
+    if exist "!DIRECTORY!\bin"^
+        set PATH=!DIRECTORY!\bin;!PATH!
+    if exist "!DIRECTORY!\jre\bin\java.exe"^
+        set PATH=!DIRECTORY!\jre\bin;!PATH!
+    if "!JAVA!" == "" (
+      if exist "!DIRECTORY!\bin\java.exe"^
+          set JAVA=!DIRECTORY!\bin
+      if exist "!DIRECTORY!\jre\bin\java.exe"^
+          set JAVA=!DIRECTORY!\jre\bin
+      if exist "!DIRECTORY!\java.exe"^
+          set JAVA=!DIRECTORY!
+    )
+  )
+)
+
+if "%JAVA%" == "" (
+  for %%d in ("%PATH:;=";"%") do (
+    set DIRECTORY=%%d
+    set DIRECTORY=!DIRECTORY:"=!
+    if not "!DIRECTORY!" == "" (
+      if exist "!DIRECTORY!\java.exe"^
+          set JAVA=!DIRECTORY!
+    ) 
+  )
+  if not "%JAVA_HOME%" == "" (
+    if exist "%JAVA_HOME%\bin\java.exe"^
+         set JAVA=%JAVA_HOME%\bin
+    if exist "%JAVA_HOME%\jre\bin\java.exe"^
+         set JAVA=%JAVA_HOME%\jre\bin
+  )
+)
+
+if not exist "%JAVA%\java.exe" (
+  echo Seanox Devwex Service [5.7.1 20241227]
+  echo Copyright ^(C^) 2024 Seanox Software Solutions
+  echo Experimental Server Engine
+  echo.
+  echo ERROR: Java Runtime Environment not found
+  goto :EOF
+)
+
+for %%f in (%RUNTIME%\*.bat %RUNTIME%\*.cmd) do call %RUNTIME%\%%f
+
+set OPTIONS=%OPTIONS% -Dlibraries=%LIBRARIES%
+set OPTIONS=%OPTIONS% -Dfile.encoding=ISO-8859-1
+
+"%JAVA%\java.exe" -cp devwex.jar %OPTIONS% com.seanox.devwex.Service %1 %2
+
+ENDLOCAL
