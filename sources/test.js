c83009e368615ffd591d2f1933fc4de08cea6864
seanox <service@seanox.com>
2025-01-22 14:28:26 +0100

    #0000 Review: Corrections and optimization

--- sources/test.js
+++ sources/test.js
@@ -4,7 +4,7 @@
  * Diese Software unterliegt der Version 2 der Apache License.
  *
  * Seanox aspect-js, fullstack for single page applications
- * Copyright (C) 2023 Seanox Software Solutions
+ * Copyright (C) 2025 Seanox Software Solutions
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
